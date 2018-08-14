328284fc60c4248414fe2f30d3de2733295260f5
Tonny Staunsbrink <tonny.staunsbrink@skyscanner.net>
2018-08-14 23:40:08 +0100

    Removal of about a third of dead code.

--- src/main.rs
+++ src/main.rs
@@ -48,18 +48,6 @@ enum WireType {
     Unknown(u8, u32)
 }
 
-fn field_id(wt :WireType) -> u32 {
-	return match wt {
-	    WireType::VarInt(id) => id,
-	    WireType::Bit64(id) => id,
-	    WireType::LengthDelimited(id) => id,
-	    WireType::StartGroup => panic!("Start Group not supported"),
-	    WireType::EndGroup => panic!("End group not supported"),
-	    WireType::Bit32(id) => id,
-	    WireType::Unknown(_, id) => id
-	}
-}
-
 #[derive(Debug, Clone)]
 enum WireField {
     VarInt(u32, u64),
@@ -192,7 +180,7 @@ fn parse_file_descriptor_proto(r :&mut Read,
         match e {
             WireField::LengthDelimited(1, v) => {
                 match String::from_utf8(v) {
-                    Ok(s) => (),//println!("OK: {:?}", s),
+                    Ok(_) => (),//println!("OK: {:?}", s),
                     Err(e)=> eprintln!("ERR: {:?}", e)
                 }
             },
@@ -321,152 +309,6 @@ trait Handle {
 }
 trait Sink<T> {
 	fn accept(&mut self, v: T);
-}
-
-struct MessageHandler<'a> {
-	//field_handlers: std::collections::HashMap<u64, Box<Handle>>// map[uint64]Handler
-	field_handlers: std::collections::HashMap<u64, &'a Handle>// map[uint64]Handler
-	// u32 -> Handler taking
-}
-impl<'a> MessageHandler<'a> {
-	fn new() -> Self {
-		MessageHandler{field_handlers: std::collections::HashMap::new()}
-	}
-	// ParseBytes will parse the bytes with the current handlers
-	// registered on this MessageHandler.
-	fn parse_bytes(&mut self, b: &mut&[u8]) {
-	    let v = [1u8, 2, 3];
-		self.handle(b); // return
-		self.handle(&mut &v[..]); // return
-	}
-	fn field(&mut self, fi: u64, h: &'a Handle) {
-		self.field_handlers.insert(fi, h);
-	}
-}
-
-impl<'a> Handle for MessageHandler<'a> {
-	fn handle(&self, r: &mut Read) { //error {
-		match read_varu64(r) { //binary.ReadUvarint(r)// error
-			Ok(field) => {
-				let field_type = field & 0x07;
-				let field_id = field >> 3;
-				match field_type {
-					0x00 => match self.field_handlers.get(&field_id) {
-						Some(h) => h.handle(r),//; err != nil {
-						None => return
-					}
-					0x01 => panic!("fixed64, sfixed64, double not supported"),
-					0x02 => panic!("string, bytes, embedded messages, packed repeated fields not supported"),
-					0x03 => panic!("start groups (deprecated) not supported"),
-					0x04 => panic!("end groups (deprecated) not supported"),
-					0x05 => panic!("fixed32, sfixed32, float not supported"),
-					_ => panic!("not supported")
-				}
-			},
-			Err(_) => return // FIXME
-		}
-	}
-}
-#[test]
-fn test_message_handler_handle() {
-	let sq = [1u64, 2, 3];
-	let mut v = vec![];
-	let mh1 = MessageHandler::new();
-	for _ in sq.iter() { // was s
-		struct AA (u64);
-		impl Sink<u64> for AA {
-			fn accept(&mut self, i :u64) {
-				self.0 = i;
-			}
-		}
-		let aa: AA = AA(2);
-		//v.push(|y: u64| y + 1);
-		let x1 = std::cell::Cell::new(0);
-		v.push(x1);
-		// let u64handler =
-        Uint64Handler{c: Box::new(aa)};
-//		//mh1.field(*s, Box::new(u64handler));
-//		mh1.field(*s, &u64handler);
-		let bytes = [8, 5];
-		mh1.handle(&mut &bytes[..]);
-	//	assert_eq!(5, x1.get())
-	}
-}
-
-
-struct Int32Handler<'a> {
-	callback: &'a Fn(i32)
-}
-
-impl<'a> Handle for Int32Handler<'a> {
-	fn handle(&self, r: &mut Read) {
-		match read_varu64(r) {
-			Ok(v) => {
-				if v <= std::u32::MAX as u64 {
-					(self.callback)(v as i32)
-				} else {
-					println!("errropr")// FIXME ignoring error
-				}
-			},
-			Err(r) => {
-				println!("error {:?}", r);
-			}
-		}
-	}
-}
-#[test]
-fn test_int32_handler_handle() {
-	let t255 = [255, 1];
-	let tm5 = [0b1111_1011, 0xFF, 0xFF, 0xFF, 0b0000_1111];
-	//let tm5 = [0x80, 0x80, 0x80, 0x80, 0b001_000];
-	let tcs = [(255i32, &t255[..]), (-5, &tm5[..])];
-	for tc in tcs.iter() {
-		let x1 = std::cell::Cell::new(0i32);
-		let h = Int32Handler{callback: &|y: i32| x1.set(y)};
-		//let bytes = [255, 1];//, 255, 255, 255, 255, 255, 255];
-		let mut bytes = tc.1;
-		h.handle(&mut bytes);
-		assert_eq!(tc.0, x1.get())
-	}
-}
-
-struct StringHandler {}
-
-struct Uint64Handler{c: Box<Sink<u64>>}
-
-
-impl Handle for Uint64Handler {
-	fn handle(&self, r: &mut Read) {// (err error) {
-		match read_varu64(r) {
-			Ok(_) => return,//self.c.accept(v), // was v
-			Err(_) => return // FIXME
-		};
-	}
-}
-
-struct Uint32Handler<'a> {
-	callback: &'a Fn(u32)
-}
-impl<'a> Handle for Uint32Handler<'a> {
-	fn handle(&self, r: &mut Read) {
-		match read_varu64(r) {
-			Ok(v) => {
-				if v <= std::u32::MAX as u64 {
-					(self.callback)(v as u32)
-				} // FIXME ignoring error
-			},
-			Err(_) => return // FIXME
-		}
-	}
-}
-#[test]
-fn test_uint32_handler_handle() {
-	let x1 = std::cell::Cell::new(0u32);
-	let h = Uint32Handler{callback: &|y: u32| x1.set(y)};
-	let bytes = [255, 1];//, 255, 255, 255, 255, 255, 255];
-	h.handle(&mut &bytes[..]);
-	assert_eq!(255, x1.get())
-	//assert_eq!(std::u32::MAX, x1.get())
 }
 
 #[derive(Debug)]
